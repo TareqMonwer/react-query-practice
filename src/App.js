a4a26a19604270c98908c7feb0a5a109e4595d1a
TareqMonwer <tareqmonwer137@gmail.com>
2021-06-26 14:36:04 +0600

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  useQuery,
+  useMutation,
+} from "react-query";
+
 
 function App() {
+
+  const createPost = async () => {
+    console.log('calling mutation....');
+    const data = await fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      body: JSON.stringify({
+        title: "foo",
+        body: "bar",
+        userId: 1,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((response) => response.json())
+      .then((json) => console.log(json));
+    
+    console.log(data);
+  }
+
+  const mutation = useMutation(createPost);
+  
+  const createSomething = (e) => {
+    mutation.mutate({
+      title: "foo",
+      body: "bar",
+      userId: 1,
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {mutation.error && (
+        <h5 onClick={() => mutation.reset()}>{mutation.error}</h5>
+      )}
+      <h1>Hello world</h1>
+
+      <button onClick={createSomething}>
+        {mutation.isLoading
+          ? "creating resource..."
+          : mutation.isSuccess
+          ? "created"
+            : "Create a new item"
+        }
+      </button>
+    </>
   );
 }
 
